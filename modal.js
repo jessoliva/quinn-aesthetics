23a3894240d4958cb1b31a6a91ae896a89b9c661
jessoliva <95152563+jessoliva@users.noreply.github.com>
2022-12-12 20:34:26 -0600

    changes before messing with additional pages

--- modal.js
+++ modal.js
@@ -1,6 +1,7 @@
 // first page element
 const firstPageEl = document.getElementById('firstPage');
 
+
 // modal element
 const modalEl = document.getElementById('modal');
 //
@@ -22,6 +23,7 @@ function closeModal() {
 }
 closeBtnEl.addEventListener('click', closeModal);
 
+
 // consultation button element
 const consultBtnEl = document.getElementById('consult-btn');
 const consultEl = document.getElementById('consultPage');
@@ -32,6 +34,9 @@ function consultation() {
     modalEl.classList.add('fade-out');
     firstPageEl.classList.add('hide');
 
+    // ONLY FOR WEEBLY
+    location.replace("https://www.w3schools.com")
+    
     setTimeout(function () {
         consultEl.classList.remove('hide');
         consultEl.classList.add('fade-in');
@@ -39,10 +44,37 @@ function consultation() {
     }, 1000 / 2);
 }
 consultBtnEl.addEventListener('click', consultation);
+// //
+// // display list
+// // dropdown button el
+// const listBtnEl = document.getElementById('listBtn');
+// function displayBodyAreas() {
+
+//     // get the list element
+//     const listEl = document.getElementById('myDropdown');
+//     // toggle classList
+//     listEl.classList.toggle('show');
+// }
+// listBtnEl.addEventListener('click', displayBodyAreas);
+
+// Close the dropdown if the user clicks outside of it
+// window.onclick = function(event) {
+//     if (!event.target.matches('.dropbtn')) {
+//         var dropdowns = document.getElementsByClassName("dropdown-content");
+//         var i;
+//         for (i = 0; i < dropdowns.length; i++) {
+//             var openDropdown = dropdowns[i];
+//             if (openDropdown.classList.contains('show')) {
+//                 openDropdown.classList.remove('show');
+//             }
+//         }
+//     }
+// }
 
 // face button element
 const faceBtnEl = document.getElementById('face-btn');
 const headBtnEl = document.getElementById('head-btn');
+const headTxtEl = document.getElementById('head-heading')
 const faceEl = document.getElementById('facePage');
 //
 // display face image
@@ -58,6 +90,7 @@ function face() {
 }
 faceBtnEl.addEventListener('click', face);
 headBtnEl.addEventListener('click', face);
+headTxtEl.addEventListener('click', face);
 
 // closeBtnEl.addEventListener('click', function() {
 //     modalEl.classList.add('hidden');
